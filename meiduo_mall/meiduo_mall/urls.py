9f899805fffd55fddc7bd512ac24a022a77ef68a
chao <zhangxc1120@qq.com>
2019-04-23 10:26:55 +0800

    展示首页界面

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include('users.urls', namespace='users')),  # 用户模块
+
+    url(r'^', include('contents.urls', namespace='contents')),  # 首页模块
 ]
