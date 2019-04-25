9dc51b458b3f05147975b72e46461561c8e8a1b3
chao <zhangxc1120@qq.com>
2019-04-25 09:46:09 +0800

    代码小优化使用常量替换魔法数字

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -8,6 +8,7 @@ from meiduo_mall.libs.captcha.captcha import captcha
 from meiduo_mall.utils.response_code import RETCODE
 import logging
 from meiduo_mall.libs.yuntongxun.sms import CCP
+from . import constants
 
 logger = logging.getLogger('django')
 
@@ -27,7 +28,7 @@ class ImageCodeView(View):
         # 创建redis连接对象
         redis_conn = get_redis_connection('verify_code')
         # 将图形验证码字符串存入到reids
-        redis_conn.setex('img_%s' % uuid, 300, text)
+        redis_conn.setex('img_%s' % uuid, constants.IMAGE_CODE_REDIS_EXPIRES, text)
         # 把生成好的图片响应给前端
         return http.HttpResponse(image, content_type='image/png')
 
@@ -52,6 +53,8 @@ class SMSCodeView(View):
         redis_conn = get_redis_connection('verify_code')
         # 根据uuid作为key 获取到redis中当前用户的图形验证值
         image_code_server = redis_conn.get('img_%s' % uuid)
+        # 删除图形验证码，让它只能用一次，防止刷
+        redis_conn.delete('img_%s' % uuid)
         # 从redis中取出来的数据都是bytes类型
 
         # 判断用户写的图形验证码和我们redis存的是否一致
@@ -63,10 +66,10 @@ class SMSCodeView(View):
         sms_code = '%06d' % randint(0, 999999)
         logger.info(sms_code)
         # 将生成好的短信验证码也存储到redis,以备后期校验
-        redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
         # 利用容联云SDK发短信
         # CCP().send_template_sms(手机号, [验证码, 提示用户验证码有效期多少分钟], 短信模板id)
-        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
         # 响应
 
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信验证码'})
