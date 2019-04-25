f66bc0bf1b73b0065405751cd757e95b133d2977
chao <zhangxc1120@qq.com>
2019-04-25 09:22:41 +0800

    短信验证码发送的基本实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from django_redis import get_redis_connection
 from django import http
+from random import randint
 
 from meiduo_mall.libs.captcha.captcha import captcha
+from meiduo_mall.utils.response_code import RETCODE
+import logging
+from meiduo_mall.libs.yuntongxun.sms import CCP
+
+logger = logging.getLogger('django')
 
 
 class ImageCodeView(View):
@@ -35,12 +41,32 @@ class SMSCodeView(View):
         """
 
         # 接收到前端 传入的 mobile, image_code, uuid
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
 
-        # 创建redis连接对象 根据uuid作为key 获取到reids中当前用户的图形验证值
+        # 判断参数是否齐全
+        if all([image_code_client, uuid]) is False:
+            return http.JsonResponse({'code': RETCODE.NECESSARYPARAMERR, 'errmsg': '缺少必传参数'})
+
+        # 创建redis连接对象
+        redis_conn = get_redis_connection('verify_code')
+        # 根据uuid作为key 获取到redis中当前用户的图形验证值
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        # 从redis中取出来的数据都是bytes类型
+
         # 判断用户写的图形验证码和我们redis存的是否一致
+        if image_code_server is None or image_code_client.lower() != image_code_server.decode().lower():
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码错误'})
 
         # 发送短信
+        # 利用随机模块生成一个6位数字
+        sms_code = '%06d' % randint(0, 999999)
+        logger.info(sms_code)
         # 将生成好的短信验证码也存储到redis,以备后期校验
+        redis_conn.setex('sms_%s' % mobile, 300, sms_code)
+        # 利用容联云SDK发短信
+        # CCP().send_template_sms(手机号, [验证码, 提示用户验证码有效期多少分钟], 短信模板id)
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
         # 响应
 
-        pass
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信验证码'})
