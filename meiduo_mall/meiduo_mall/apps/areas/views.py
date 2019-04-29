2996f409e6fa72c0986fcd85b35a53adf3bccffa
chao <zhangxc1120@qq.com>
2019-04-29 09:43:21 +0800

    查询所有省数据实现

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django import http
+
+from .models import Area
+from meiduo_mall.utils.response_code import RETCODE
 
 
 # Create your views here.
@@ -8,4 +12,25 @@ class AreasView(View):
 
     def get(self, request):
         """实现省市区查询逻辑"""
-        pass
+        # 获取area_id
+        area_id = request.GET.get('area_id')
+
+        # 如果area_id 没有值代表要查询所有省数据
+        if area_id is None:
+
+            # 获取所有省的数据
+            provinces_model_qs = Area.objects.filter(parent=None)
+            # provinces_model_qs = Area.objects.filter(parent__isnull=True)
+            provinces_list = []  # 把一个一个省的字典添加到此列表
+            # 遍历所有省的查询集
+            for province in provinces_model_qs:
+                province_dict = {
+                    'id': province.id,
+                    'name': province.name
+                }
+                provinces_list.append(province_dict)
+
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': provinces_list})
+        else:
+            # 如果area_id 有值代表要查询指定省及下级行政区,查询指定市及下级行政区
+            pass
