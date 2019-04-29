a2a84e1f6dfd4e9ed3e7fdede633d3be1749ef8a
chao <zhangxc1120@qq.com>
2019-04-29 10:18:44 +0800

    实现省市区数据缓存

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django import http
+from django.core.cache import cache
 
 from .models import Area
 from meiduo_mall.utils.response_code import RETCODE
@@ -18,41 +19,51 @@ class AreasView(View):
         # 如果area_id 没有值代表要查询所有省数据
         if area_id is None:
 
-            # 获取所有省的数据
-            provinces_model_qs = Area.objects.filter(parent=None)
-            # provinces_model_qs = Area.objects.filter(parent__isnull=True)
-            provinces_list = []  # 把一个一个省的字典添加到此列表
-            # 遍历所有省的查询集
-            for province in provinces_model_qs:
-                province_dict = {
-                    'id': province.id,
-                    'name': province.name
-                }
-                provinces_list.append(province_dict)
+            # 先尝试性的去redis获取所有省的数据
+            provinces_list = cache.get('provinces_list')
+            if provinces_list is None:
+                # 获取所有省的数据
+                provinces_model_qs = Area.objects.filter(parent=None)
+                # provinces_model_qs = Area.objects.filter(parent__isnull=True)
+                provinces_list = []  # 把一个一个省的字典添加到此列表
+                # 遍历所有省的查询集
+                for province in provinces_model_qs:
+                    province_dict = {
+                        'id': province.id,
+                        'name': province.name
+                    }
+                    provinces_list.append(province_dict)
+
+                # 如果没有缓存,此时应该把所有省数据缓存起来
+                cache.set('provinces_list', provinces_list, 3600)
 
             return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': provinces_list})
         else:
             # 如果area_id 有值代表要查询指定省及下级行政区,查询指定市及下级行政区
-            try:
-                area_model = Area.objects.get(id=area_id)  # 此模型有可能是某个省,也有可能是某个市
-            except Area.DoesNotExist:
-                return http.HttpResponseForbidden('area_id不存在')
 
-            subs_model_qs = area_model.subs.all()  # 获取下级所有行政区
+            area_datas= cache.get('area_datas_%s' % area_id)
+            if area_datas is None:
+                try:
+                    area_model = Area.objects.get(id=area_id)  # 此模型有可能是某个省,也有可能是某个市
+                except Area.DoesNotExist:
+                    return http.HttpResponseForbidden('area_id不存在')
 
-            sub_list = []  # 用来装下级行政区字典
-            for sub in subs_model_qs:
-                sub_dict = {
-                    'id': sub.id,
-                    'name': sub.name
+                subs_model_qs = area_model.subs.all()  # 获取下级所有行政区
+
+                sub_list = []  # 用来装下级行政区字典
+                for sub in subs_model_qs:
+                    sub_dict = {
+                        'id': sub.id,
+                        'name': sub.name
+                    }
+                    sub_list.append(sub_dict)
+
+                area_datas = {
+                    'id': area_model.id,
+                    'name': area_model.name,
+                    'subs': sub_list
                 }
-                sub_list.append(sub_dict)
-
-            area_datas = {
-                'id': area_model.id,
-                'name': area_model.name,
-                'subs': sub_list
-            }
+                cache.set('area_datas_%s' % area_id, area_datas, 3600)
 
             return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'sub_data': area_datas})
             # {
