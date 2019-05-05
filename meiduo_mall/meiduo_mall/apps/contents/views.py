abce752e324dc5d3e16f6a1a1022dc18d928f490
chao <zhangxc1120@qq.com>
2019-05-05 09:27:15 +0800

    首页商品分类数据结构分析

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -6,4 +6,31 @@ class IndexView(View):
     """首页"""
 
     def get(self, request):
-        return render(request, 'index.html')
+        """
+        商品分类及广告数据展示
+        """
+        categories = {}  # 用来包装所有商品类别数据
+        """
+        { 
+            1: {
+                'channels': [cat1-1, cat1-2,...]  第一组里面的所有一级数据
+                'sub_cats': [cat2-1, cat2-2]   用来装所有二级数据, 在二级中再包含三级数据cat2-1.sub_cats = cat3
+                },
+                
+            2: {
+                'channels': [cat1-1...],
+                'sub_cats': [...]
+                }
+                
+        
+        }
+        """
+
+
+
+        context = {
+            'categories': ''
+        }
+
+
+        return render(request, 'index.html', context)
