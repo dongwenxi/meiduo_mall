ba713bebd4ccfcb61af6e6d616a73c2aaccc0694
chao <zhangxc1120@qq.com>
2019-05-14 12:56:56 +0800

    商品详情页面静态化

--- meiduo_mall/meiduo_mall/apps/goods/utils.py
+++ meiduo_mall/meiduo_mall/apps/goods/utils.py
@@ -1,1 +1,1 @@
-def get_breadcrumb(category):    """面包屑导航"""    breadcrumb = {        'cat1': category.parent.parent,        'cat2': category.parent,        'cat3': category    }    return breadcrumb
+def get_breadcrumb(category):    """面包屑导航"""    cat1 = category.parent.parent    cat1.url = cat1.goodschannel_set.all()[0].url    breadcrumb = {        'cat1': cat1,        'cat2': category.parent,        'cat3': category    }    return breadcrumb
